2d768582a0e9e26e1eedb885a5796d78752c13bf
fred <abnertorres58@gmail.com>
2022-05-03 01:24:02 -0400

    Get a single Customer

--- src/main/java/com/freudromero/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/freudromero/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import com.freudromero.springdemo.entity.Customer;
 import com.freudromero.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,12 @@ public class CustomerRestController {
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    // Add mapping for GET /customers/{customerId}
+    @GetMapping("customers/{customerId}")
+    public Customer getCustomer (@PathVariable int customerId) {
+        Customer theCustomer = customerService.getCustomer(customerId);
+        return theCustomer;
     }
 }
